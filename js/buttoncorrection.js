89c9ff9157e9aa3a9840af5ef6d567b70981294d
Adrik1137 <Adrik1137@gmail.com>
2021-04-19 21:19:28 -0500

    Borrador inicializado
    Importado la plantilla de Bootstrap y configurado, comencé a cambiar algo del texto

---
+++ js/buttoncorrection.js
@@ -0,0 +1,5 @@
+
+document.getElementById("tagscr").addEventListener("load", function(event){
+    alert("loaded");
+    document.getElementById("appendBtn").setAttribute("style", "cursor: pointer; border: 1px solid white; display: inline; margin-bottom: 20px; background-color: black; color: white;")
+})
